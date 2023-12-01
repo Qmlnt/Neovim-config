8141c6c4fc76ed3dd78720d56aeb74044ea21888
Qmlnt <qmelint@gmail.com>
2023-12-01 13:47:32 +0200

    treesitter-textobjects start

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,12 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
         build = ":TSUpdate",
-        event = "VeryLazy", -- wait till UI loads
+        event = "VeryLazy", -- don't slow down startup
         opts = {
-            highlight = { enable = true },
-            indent = { enable = true },
             auto_install = true,
-            sync_install = false, -- only for ensure_installed
+            sync_install = false,
             ensure_installed = {
                 "arduino",
                 "bash",
@@ -38,11 +37,37 @@ return {
                 "vimdoc",
                 "yaml"
             },
+            -- MODULES
+            highlight = { enable = true },
+            indent = { enable = true }, -- for the `=` formatting
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<tab>",
+                    node_incremental = "<tab>",
+                    node_decremental = "<s-tab>",
+                    scope_incremental = "<c-tab>",
+                },
+            },
+            textobjects = {
+                select = { -- TODO
+                    enable = false,
+                    lookahead = true,
+                    keymaps = {},
+                },
+                swap = {
+                    enable = true,
+                    swap_next = { ["<leader>s"] = "@parameter.inner" },
+                    swap_previous = { ["<leader>S"] = "@parameter.inner" },
+                },
+                move = { -- TODO
+                    enable = false,
+                    set_jumps = true,
+                },
+            }
         },
         config = function(_, opts)
             require("nvim-treesitter.configs").setup(opts)
         end,
     },
-
-
 }
