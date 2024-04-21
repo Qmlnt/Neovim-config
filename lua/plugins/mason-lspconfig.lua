aea4b5a5a4956fa31dc3fe5a6938c8220e53fada
Qmlnt <qmelint@gmail.com>
2024-04-21 17:07:25 +0300

    use clippy in rust-analyzer

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -12,20 +12,20 @@ local M = {
 
 M.opts = {
     ensure_installed = {
-        "arduino_language_server",
-        "bashls",
-        "clangd", -- C/C++
-        "cmake",
-        --"csharp_ls", -- piece of 󰱵
-        "hls", -- Haskell
-        "taplo", -- toml
-        "jsonls",
         "lua_ls",
-        "ruff_lsp", -- Python
+        "bashls",
         "rust_analyzer",
+        -- "arduino_language_server",
+        -- "clangd", -- C/C++
+        -- "cmake",
+        --"csharp_ls", -- piece of 󰱵
+        -- "hls", -- Haskell
+        -- "taplo", -- toml
+        -- "jsonls",
+        -- "ruff_lsp", -- Python
     },
 
-    handlers = { -- automatic server setup
+    handlers = {              -- automatic server setup
         function(server_name) -- default handler
             package.loaded.lspconfig[server_name].setup {}
         end
@@ -38,6 +38,41 @@ M.opts.handlers.lua_ls = function()
             Lua = {
                 diagnostics = { globals = { "vim" } }
             }
+        }
+    }
+end
+
+M.opts.handlers.rust_analyzer = function() -- TODO
+    package.loaded.lspconfig.rust_analyzer.setup {
+        settings = {
+            ["rust-analyzer"] = {
+                cargo = {
+                    allFeatures = true,
+                    loadOutDirsFromCheck = true,
+                    runBuildScripts = true,
+                },
+                -- Add clippy lints for Rust.
+                checkOnSave = {
+                    allFeatures = true,
+                    command = "clippy",
+                    extraArgs = {
+                        "--",
+                        "--no-deps",
+                        "-Dclippy::correctness",
+                        "-Dclippy::complexity",
+                        "-Wclippy::perf",
+                        "-Wclippy::pedantic",
+                    },
+                },
+                procMacro = {
+                    enable = true,
+                    ignored = {
+                        ["async-trait"] = { "async_trait" },
+                        ["napi-derive"] = { "napi" },
+                        ["async-recursion"] = { "async_recursion" },
+                    },
+                },
+            },
         }
     }
 end
